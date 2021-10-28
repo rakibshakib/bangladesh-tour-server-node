d00f1302008b552050819b1c2f2d29209b5b5f0e
rakibshakib <rakibshakib74@gmail.com>
2021-10-29 00:45:11 +0600

    update cors

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-// mongoSetting 
+// mongoDBSetting 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tygmv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 async function run(){ 
     try {
         await client.connect();
